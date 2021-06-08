864284abee4f6d1e4fa1f597be12541db1ce69f5
Garrett Stonis <stonisg@wit.edu>
2021-06-08 00:59:41 -0400

    Began setting up server.

--- Camera Bot - Server/Program.cs
+++ Camera Bot - Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,15 @@ namespace Camera_Bot___Server
     {
         static void Main(string[] args)
         {
+            byte[] buffer = new byte[1024];
 
+
+            //Establishing local endpoint.
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress IP_address = hostEntry.AddressList[0];
+            IPEndPoint local_endPoint = new IPEndPoint(IP_address, 11000);
+
+            //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-server-socket-example
         }
     }
 }
