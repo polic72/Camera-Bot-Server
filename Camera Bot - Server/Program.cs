fbb5b6ab5b2feed18ffa49dface6ca7a0cc8a7ed
Garrett Stonis <stonisg@wit.edu>
2021-06-09 01:53:32 -0400

    Made more progress on the testing code.

--- Camera Bot - Server/Program.cs
+++ Camera Bot - Server/Program.cs
@@ -13,6 +13,9 @@ namespace Camera_Bot___Server
     {
         static void Main(string[] args)
         {
+            //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-server-socket-example
+
+
             byte[] buffer = new byte[1024];
 
 
@@ -21,7 +24,35 @@ namespace Camera_Bot___Server
             IPAddress IP_address = hostEntry.AddressList[0];
             IPEndPoint local_endPoint = new IPEndPoint(IP_address, 11000);
 
-            //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-server-socket-example
+            IPAddress address = new IPAddress(new byte[] { 10, 0, 0, 68 });
+
+            Socket listener = new Socket(IP_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(local_endPoint);
+                listener.Listen(10);
+
+
+                while (true)
+                {
+                    Socket client = listener.Accept();
+
+                    int count = client.Receive(buffer);
+
+
+                    string test = Encoding.ASCII.GetString(buffer, 0, count);
+
+
+                    //client.Send()
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
