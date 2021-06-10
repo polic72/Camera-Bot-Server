a1c6220ca76ea797c31be89f896f04a180729d00
Garrett Stonis <stonisg@wit.edu>
2021-06-10 01:41:40 -0400

    Synchronous server "done".

--- Camera Bot - Server/Program.cs
+++ Camera Bot - Server/Program.cs
@@ -18,13 +18,15 @@ namespace Camera_Bot___Server
 
             byte[] buffer = new byte[1024];
 
+            int counter = 0;
+
 
             //Establishing local endpoint.
-            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress IP_address = hostEntry.AddressList[0];
-            IPEndPoint local_endPoint = new IPEndPoint(IP_address, 11000);
+            //IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress IP_address = hostEntry.AddressList[0];
 
-            IPAddress address = new IPAddress(new byte[] { 10, 0, 0, 68 });
+            IPAddress IP_address = new IPAddress(new byte[] { 10, 0, 0, 68 });
+            IPEndPoint local_endPoint = new IPEndPoint(IP_address, 11000);
 
             Socket listener = new Socket(IP_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
@@ -38,15 +40,62 @@ namespace Camera_Bot___Server
                 {
                     Socket client = listener.Accept();
 
-                    int count = client.Receive(buffer);
+                    while (client.Connected)
+                    {
+                        int count = -1;
+                        try
+                        {
+                            count = client.Receive(buffer);
+                        }
+                        catch (SocketException)
+                        {
+                            break;
+                        }
 
 
-                    string test = Encoding.ASCII.GetString(buffer, 0, count);
+                        string command = Encoding.ASCII.GetString(buffer, 0, count);
+
+                        switch (command)
+                        {
+                            case "Up":
+                                client.Send(Encoding.ASCII.GetBytes("Ok"));
+                                Console.WriteLine(counter++);
+                                break;
 
 
-                    //client.Send()
-                    client.Shutdown(SocketShutdown.Both);
-                    client.Close();
+                            case "Down":
+                                //
+                                break;
+
+
+                            case "Left":
+                                //
+                                break;
+
+
+                            case "Right":
+                                //
+                                break;
+
+
+                            case "Break":
+                                client.Send(Encoding.ASCII.GetBytes("Goodbye"));
+
+                                client.Shutdown(SocketShutdown.Both);
+                                client.Close();
+
+
+                                //listener = new Socket(IP_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                                //listener.Bind(local_endPoint);
+                                //listener.Listen(10);
+                                break;
+
+
+                            default:
+                                client.Send(Encoding.ASCII.GetBytes("Bad"));
+                                continue;
+                        }
+                    }
                 }
             }
             catch (Exception e)
