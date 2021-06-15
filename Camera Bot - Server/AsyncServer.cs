4b009a4132b2eab1bb7b49d525bc32fbfc609b40
Garrett Stonis <GStonis@emduggan.com>
2021-06-15 13:58:32 -0400

    Just barely beginning the AsyncServer code.

---
+++ Camera Bot - Server/AsyncServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camera_Bot___Server
+{
+    /// <summary>
+    /// Represent an asynchronous server.
+    /// </summary>
+    public class AsyncServer
+    {
+        public AsyncServer()
+        {
+
+        }
+    }
+}
