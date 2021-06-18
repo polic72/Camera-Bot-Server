da2bf9df55506d40f48a06872722210f37678946
Garrett Stonis <stonisg@wit.edu>
2021-06-18 02:47:59 -0400

    Added ConfigFile. Started implementing the server as it should work properly.

---
+++ Camera Bot - Server/ConfigFile.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace Camera_Bot___Server
+{
+    /// <summary>
+    /// Represents a config file for storage and usage.
+    /// </summary>
+    public class ConfigFile
+    {
+        /// <summary>
+        /// The character that represent a commented line.
+        /// </summary>
+        public const char CommentCharacter = '#';
+
+
+        #region Properties
+
+        /// <summary>
+        /// The regex that matches config lines.
+        /// </summary>
+        public static Regex ConfigLine_Regex { get; }
+
+
+        /// <summary>
+        /// The path of the file to read/write to.
+        /// </summary>
+        public string Path { get; }
+
+
+        /// <summary>
+        /// The options recognized by the config file. Can be changed, but must call <see cref="Camera_Bot___Server.ConfigFile.WriteFile"/> 
+        /// to sync the data.
+        /// </summary>
+        public Dictionary<string, string> ConfigOptions { get; }
+
+        #endregion Properties
+
+
+        #region Constructors
+
+        static ConfigFile()
+        {
+            ConfigLine_Regex = new Regex(@"^(.+)=(.+)$", RegexOptions.Compiled);
+        }
+
+
+        /// <summary>
+        /// Constructs a config file with the given path.
+        /// </summary>
+        /// <param name="path">The path of the file to read/write to.</param>
+        public ConfigFile(string path)
+        {
+            Path = path;
+
+            ConfigOptions = new Dictionary<string, string>(10);
+        }
+
+        #endregion Constructors
+
+
+        #region File IO
+
+        /// <summary>
+        /// Reads the file and updates the options.
+        /// </summary>
+        /// <remarks>Ignores empty lines and lines that start with the <see cref="Camera_Bot___Server.ConfigFile.CommentCharacter"/>. 
+        /// Also ignores any malformed lines.</remarks>
+        public void ReadFile()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(Path);
+
+                foreach (string line in lines)
+                {
+                    if (line != "" && !line.StartsWith(CommentCharacter.ToString()))
+                    {
+                        Match match = ConfigLine_Regex.Match(line);
+
+                        if (match.Success)
+                        {
+                            if (ConfigOptions.ContainsKey(match.Groups[1].Value))
+                            {
+                                ConfigOptions[match.Groups[1].Value] = match.Groups[2].Value;
+                            }
+                            else
+                            {
+                                ConfigOptions.Add(match.Groups[1].Value, match.Groups[2].Value);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                //Do nothing.
+            }
+        }
+
+
+        /// <summary>
+        /// Writes the stored data into the file. If the file already exists, its values are edited.
+        /// </summary>
+        public void WriteFile()
+        {
+            if (File.Exists(Path))
+            {
+                string[] lines = File.ReadAllLines(Path);
+
+                StringBuilder builder = new StringBuilder(lines.Sum(x => x.Length));
+
+                foreach (string line in lines)
+                {
+                    bool found = false;
+
+                    foreach (string key in ConfigOptions.Keys)
+                    {
+                        if (line.StartsWith(key))
+                        {
+                            string new_line = line.Substring(0, line.LastIndexOf('=') + 1) + ConfigOptions[key] + "\r\n";
+
+                            builder.Append(new_line);
+
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        builder.Append(line + "\r\n");
+                    }
+                }
+
+
+                using (StreamWriter writer = new StreamWriter(Path))
+                {
+                    writer.Write(builder.ToString());
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(Path))
+                {
+                    foreach (string key in ConfigOptions.Keys)
+                    {
+                        writer.WriteLine(key + "=" + ConfigOptions[key]);
+                    }
+                }
+            }
+        }
+
+        #endregion File IO
+    }
+}
--- Camera Bot - Server/Program.cs
+++ Camera Bot - Server/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO.Ports;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -12,9 +12,30 @@ namespace Camera_Bot___Server
 {
     class Program
     {
+        public const string CONFIG_FILE = "server.config";
+
         static void Main(string[] args)
         {
-            //
+            ConfigFile configFile = new ConfigFile(CONFIG_FILE);
+
+            configFile.ConfigOptions.Add("IPv4", "");
+            configFile.ConfigOptions.Add("Port", "11000");
+            configFile.ConfigOptions.Add("SerialPortName", "");
+
+
+            if (!File.Exists(CONFIG_FILE))
+            {
+                configFile.WriteFile();
+
+                return;
+            }
+
+
+            configFile.ReadFile();
+
+            //Check for correct config options.
+            //Actually start server with them.
+            //Optionally make a loop to type commands in the terminal.
         }
     }
 }
